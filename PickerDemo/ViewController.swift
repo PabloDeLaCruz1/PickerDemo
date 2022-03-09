c9628f388f096cb7a24483b2334a0d96271c4b12
PabloDeLaCruz1 <pablo.de.la.cruz.pro@gmail.com>
2022-03-09 16:28:49 -0500

    initial commit

--- PickerDemo/ViewController.swift
+++ PickerDemo/ViewController.swift
@@ -7,10 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var pickerData = [["1","22222","3","4"],["Hello", "Noooo", "ok ok ok "]]
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData[component].count
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+        
+    }
 
+    @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
